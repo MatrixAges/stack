5ca0886ca4ca5c4f4b7d6421d7921f24904d13f5
1yasa <xiewendao@gmail.com>
2025-10-24 15:49:03 +0800

    chore: sync ai-sdk-panel

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,35 @@ const is_dev = process.env.NODE_ENV === 'development'
 const config: NextConfig = {
 	reactStrictMode: false,
 	devIndicators: false,
-	outputFileTracingRoot: __dirname
+	outputFileTracingRoot: __dirname,
+	turbopack: {
+		rules: {
+			'*.svg': {
+				as: '*.js',
+				loaders: [
+					{
+						loader: '@svgr/webpack',
+						options: {
+							svgo: true,
+							svgoConfig: {
+								plugins: [
+									{
+										name: 'preset-default',
+										params: {
+											overrides: {
+												removeViewBox: false
+											}
+										}
+									},
+									'prefixIds'
+								]
+							}
+						}
+					}
+				]
+			}
+		}
+	}
 }
 
 export default config
