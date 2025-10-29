eb7f1828e4522cbe70dd85d342612c9c6a6b60b4
1yasa <xiewendao@gmail.com>
2025-10-29 19:20:27 +0800

    chore: sync ai-sdk-panel

--- app/ai-sdk-panel/page.tsx
+++ app/ai-sdk-panel/page.tsx
@@ -4,8 +4,25 @@ import type { Metadata } from 'next'
 
 const Index = () => {
 	return (
-		<div className='flex justify-center w-screen min-h-screen py-20'>
+		<div
+			className='
+				flex flex-col items-center
+				w-screen min-h-screen
+				py-20
+			'
+		>
 			<Client />
+			<a
+				className='
+					pt-12
+					text-xsm text-gray
+					underline underline-offset-2
+				'
+				href='https://github.com/matrixages/ai-sdk-panel'
+				target='_blank'
+			>
+				Github
+			</a>
 		</div>
 	)
 }
